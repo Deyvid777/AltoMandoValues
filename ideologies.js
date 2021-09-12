169353374076d4f766058df76d4896f5d0d33eba
Sr Deyvid <72520742+Deyvid777@users.noreply.github.com>
2021-09-12 13:35:43 +0200

    Create ideologies.js

---
+++ ideologies.js
@@ -0,0 +1,13 @@
+ideologies = [
+    {
+        "name": "WIP",
+                "desc": "En Proceso..."
+                "theory":"Seré feliz si incluso lees en general.",
+        "stats": {
+            "econ": 30,
+            "dipl": 35,
+            "govt": 95,
+            "scty": 50,
+        }
+    }
+];
